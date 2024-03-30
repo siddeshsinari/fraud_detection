672930ffd607868898be847dd144199f1e542a3a
siddeshsinari <siddesh.sinari@gmail.com>
2024-03-30 23:32:00 +0530

    corrected cofe for sum function

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,14 @@
 print ('adding all the data processing code related to fraud detection')
+
+a = 5
+b = 7
+
+def sum(a,b):
+    c = a+b
+    return c
+
+def mult(a,b):
+    d = a*b
+    return b
+
+print ('multiplication function')
